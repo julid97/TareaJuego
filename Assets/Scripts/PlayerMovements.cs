3d144829fed42c20e97e512dc64e682a9c77d4fe
julid97 <julid_2010@hotmail.com.ar>
2025-08-31 20:53:34 -0300

    Add Animations Idle and Walk

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class PlayerMovements : MonoBehaviour
 {
     public float speed;
+    public Animator animator;
+
     private Rigidbody2D _Rb;
     private float _horizontalDirection;
+    private SpriteRenderer _sprite;
 
     void Start()
     {
         _Rb = GetComponent<Rigidbody2D>();
+        _sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -17,5 +21,19 @@ public class PlayerMovements : MonoBehaviour
         _horizontalDirection = Input.GetAxisRaw("Horizontal");
 
         _Rb.linearVelocityX = _horizontalDirection * speed;
+
+        animator.SetFloat("movement", _Rb.linearVelocity.x);
+
+        if (_horizontalDirection != 0)
+        {
+            if (_horizontalDirection < 0)
+            {
+                _sprite.flipX = true; 
+            }
+            else if (_horizontalDirection > 0)
+            {
+                _sprite.flipX = false; 
+            }
+        }
     }
 }

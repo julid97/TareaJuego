cb2b4124f4e76be8a1aaa9a87149f8dd500391fe
julid97 <julid_2010@hotmail.com.ar>
2025-08-30 22:24:38 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D _Rb;
+    private float _horizontalDirection;
+
+    void Start()
+    {
+        _Rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+}
